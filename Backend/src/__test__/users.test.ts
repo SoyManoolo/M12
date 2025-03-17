aa07c7dc317f273fa2c6ffae2d6e2e59c8ee0f82
SoyManoolo <erik.saldi.diaz@gmail.com>
2025-03-17 18:20:28 +0100

    add dependencies for logging and testing and first test configuration

---
+++ Backend/src/__test__/users.test.ts
@@ -0,0 +1,17 @@
+import supertest from "supertest";
+import { app } from "../app";
+import { sequelize } from "../config/database";
+
+const api = supertest(app)
+
+afterAll(async () => {
+    await sequelize.close
+})
+
+describe('Users test:', () => {
+    test('Test de prueba', async () => {
+        await api
+            .get('')
+            .expect(200)
+    })
+})
